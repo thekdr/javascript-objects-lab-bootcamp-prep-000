bf42afe109d1a0e934fe28422e4e17f5eaf240ab
Kristopher Ross <thekdr@yahoo.com>
2018-02-01 19:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var recipe = {
 };
 
 function updateObjectWithKeyAndValue(obj, key, val) {
-  var newObject = Object.assign({}, obj, {key: val});
+  var newObject = Object.assign({}, obj, {[key]: val});
   return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
-  return Object.assign(obj, {key: val});
+  return Object.assign(obj, {[key]: val});
 }
 
 function deleteFromObjectByKey(obj, key) {
